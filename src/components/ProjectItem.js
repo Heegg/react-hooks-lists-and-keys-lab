e3103c3a8de0670bfee342e65879267b2e23f32f
Heeji Hyun <wlgmlgus@gmail.com>
2023-03-20 05:25:26 -0400

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -9,39 +9,36 @@ function ProjectItem({ name, about, technologies }) {
         <p>{about}</p>
         <div className="technologies">
           {/* render a <span> for each technology in the technologies array */}
-        <span>{technologies[0]}</span>
-        <span>{technologies[1]}</span>
-        <span>{technologies[2]}</span>
+        <span>{technologies}</span>
+        {
+        technologies.map(technology => 
+        <span key={technology}>{technology}</span>)
+        }
         </div>
       </div>
     </>
     );
 }
-// console.log(ProjectItem)
+
 
 export default ProjectItem;
 
 
-    {/* <>
-    <div className="project-item">
-      <h3>{name}Reciplease</h3>
-      <p>{about}A recipe tracking app</p>
-      <div className="technologies">
-        <span>{technologies}Rails</span> <span>Bootstrap CSS</span>
-      </div>
-    </div>
-        <div className="project-item">
-        <h3>{name}Kibbles N Bitz</h3>
-        <p>{about}Tinder for dogs</p>
-        <div className="technologies">
-          <span>{technologies}React</span> <span>Redux</span>
-        </div>
-      </div>
-          <div className="project-item">
-          <h3>{name}Alienwares</h3>
-          <p>{about}Etsy for aliens</p>
-          <div className="technologies">
-            <span>{technologies}React</span> <span>Redux</span> <span>Rails</span>
-          </div>
-        </div>
-    </> */}
+// function ProjectItem({ name, about, technologies }) {
+//   return (
+//     <>
+//       <div className="project-item">
+//         <h3>{name}</h3>
+//         <p>{about}</p>
+//         <div className="technologies">
+//           {/* render a <span> for each technology in the technologies array */}
+//         <span>{technologies[0]}</span>
+//         <span>{technologies[1]}</span>
+//         <span>{technologies[2]}</span>
+//         </div>
+//       </div>
+//     </>
+//     );
+// }
+
+
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,13 +3,6 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 
-// const projectName = projects.map((project) => {
-//   return <li key={project.id}>{project.name} {project.about} {project.technologies}</li>
-//  })
-
-//  const projectItems = projects.map((project) => {
-//   return <ProjectItem>
-//  })
 
 function ProjectList({ projects }) {
 console.log(projects);
@@ -20,29 +13,51 @@ console.log(projects);
 
       <div id="project-list">
       {/* render ProjectItem components here */}
-
-      <ProjectItem 
-      name={projects[0].name}
-      about={projects[0].about}
-      technologies={projects[0].technologies}
-      />
-      <ProjectItem 
-      name={projects[1].name}
-      about={projects[1].about}
-      technologies={projects[1].technologies}
-      /> 
-      <ProjectItem 
-      name={projects[2].name}
-      about={projects[2].about}
-      technologies={projects[2].technologies}
-      /> 
-
-      {/* <h3 key={projects.id}>{projects.name} {projects.about} {projects.technologies}</h3> */}
+      {projects.map((project) => {
+        return <ProjectItem 
+        key={project.id} 
+        name={project.name} 
+        about={project.about}
+        technologies={project.technologies}
+        />
+      })}
       
       </div>
     </div>
   );
 }
 
-
 export default ProjectList;
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+  
+//     return (
+//       <div id="projects">
+//         <h2>My Projects</h2>
+  
+//         <div id="project-list">
+//         {/* render ProjectItem components here */}
+  
+//         <h3 key={projects.id}>{projects.name} {projects.about} {projects.technologies}</h3>
+  
+//         <ProjectItem 
+//         name={projects[0].name}
+//         about={projects[0].about}
+//         technologies={projects[0].technologies}
+//         />
+//         <ProjectItem 
+//         name={projects[1].name}
+//         about={projects[1].about}
+//         technologies={projects[1].technologies}
+//         /> 
+//         <ProjectItem 
+//         name={projects[2].name}
+//         about={projects[2].about}
+//         technologies={projects[2].technologies}
+//         />
+//         </div>
+//       </div>
+//     );
+//   }
+  
